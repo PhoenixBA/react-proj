b3a3d08654d75f5b2562921a46ebe434bc03881f
Aleksandra Bukina <87126210+PhoenixBA@users.noreply.github.com>
2021-12-21 11:24:13 +0200

    firstcommit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,69 @@
-import logo from './logo.svg';
+import React, {useState} from "react";
+import DisplayComponent from './Components/DisplayComponent';
+import BtnComponent from './Components/BtnComponent';
 import './App.css';
 
 function App() {
+  const [time, setTime] = useState({ ms: 0, s: 0, m: 0, h: 0 });
+  const [interval, setInterv] = useState();
+  const [status, setStatus] = useState(0);
+
+  // not started = 0
+  // started = 1
+  // paus = 2
+
+  const start = () => {
+    run();
+    setStatus(1);
+    setInterv(setInterval(run, 10));
+  };
+
+  var updatedMs = time.ms, updatedS = time.s, updatedM = time.m, updatedH = time.h;
+  const run = () => {
+    if(updatedM === 59) {
+      updatedH++;
+      updatedM = 0;
+    }
+    if(updatedS === 59) {
+      updatedM++;
+      updatedS = 0;
+    }
+    if (updatedMs === 99) {
+      updatedS++;
+      updatedMs = 0;
+    }
+    updatedMs++;
+    return setTime({ms:updatedMs, s:updatedS, m:updatedM, h:updatedH});
+  };
+  
+  const stop = () => { 
+    clearInterval(interval);
+    setStatus(2);
+  };
+
+  const reset = () => { 
+    clearInterval(interval);
+    setStatus(0);
+    setTime({ms:0, s:0, m:0, h:0});
+  };
+  
+  const wait = () => start();
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="main-section">
+      <div className="clock-holder">
+        <div className="stopwatch">
+          <DisplayComponent time={time}/>
+          <BtnComponent status={status} wait={wait} reset={reset} stop={stop} start={start}/>
+        </div>
+      </div>
     </div>
   );
 }
 
 export default App;
+
+
+// const source = Rx.Observable
+//   .interval(100 /* ms */ )
+//   .timeInterval();
